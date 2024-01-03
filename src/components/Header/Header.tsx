bf8d934ecb65cd93866efbc79845c02a1f39e7f2
abvictor <victor.amaralb@hotmail.com>
2024-01-03 20:56:41 -0300

    feat: create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import bgDesktop from '../../assets/bg-image.png'
+
+function Header () {   
+    return (
+        <header className="w-full bg-slate-500">
+            <img className='w-full h-48 object-cover z-10' src={bgDesktop}/>
+        </header>
+    )
+}
+
+export default Header;

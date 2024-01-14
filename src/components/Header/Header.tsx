edbb2eee66fffef48eac44429b9679e97e83a7b1
abvictor <victor.amaralb@hotmail.com>
2024-01-14 11:16:02 -0300

    style: responsive header fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import bgDesktop from '../../assets/bg-image.png'
 
 function Header () {   
     return (
-        <header className="w-full bg-slate-500">
+        <header className="w-full bg-slate-500 ">
             <img className='w-full h-48 object-cover z-10' src={bgDesktop}/>
         </header>
     )

22c19a7769f45e0b79833ee7247bdf08517c8bcc
abvictor <victor.amaralb@hotmail.com>
2024-01-15 20:49:39 -0300

    style: provide new header size and alt description

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import bgDesktop from '../../assets/bg-image.png'
 function Header () {   
     return (
         <header className="w-full bg-slate-500 ">
-            <img className='w-full h-48 object-cover z-10' src={bgDesktop}/>
+            <img className='w-full h-60 object-cover z-10' src={bgDesktop} alt='An blue background-image with white stripes'/>
         </header>
     )
 }

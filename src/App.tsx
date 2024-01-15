236c35132ebd21680373bb922d3fad47e1b9cef0
abvictor <victor.amaralb@hotmail.com>
2024-01-15 20:08:49 -0300

    style: fix responsive map margin

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import Header from "./components/Header/Header"
 
 import { ChevronRight } from "lucide-react"
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import './app.css'
 import "leaflet/dist/leaflet.css"
 
 import { IResults } from '../src/@types/types'
-
 
 function App() {
   const [valueSearch, setValueSearch] = useState('');
@@ -44,17 +43,19 @@ function App() {
     <main className="flex flex-col items-center w-full h-full">
         <Header />
 
-      <div className="flex flex-col w-full items-center justify-center -mt-40">
+      <div className="flex flex-col w-full items-center justify-center absolute mt-4">
         <span className="text-white font-medium size text-4xl mb-3">IP Address Tracker</span>
           <div className="flex w-full justify-center">
-            <input value={valueSearch.trim()} onChange={(e) => setValueSearch(e.target.value)} placeholder="Search for any IP address or domain" className="max-md:w-[60%] w-[25%] h-12 indent-4 rounded-l-xl max-md:text-sm "/>
+            <input value={valueSearch.trim()} onChange={(e) => setValueSearch(e.target.value)} placeholder="Search for any IP address or domain" className="max-lg:w-[50%] w-[25%] h-12 indent-4 rounded-l-xl max-md:text-xs max-md:indent-2"/>
                 <button onClick={handleSearchInputValue} className="flex items-center justify-center w-12 h-12 bg-black rounded-r-xl">
                   <ChevronRight size={24} className="text-white" />
                 </button>
           </div>
-      </div>
+       </div>
 
-      <section className="sm:flex-col lg:flex w-[70%] sm:h-auto lg:h-36 justify-center bg-white rounded-lg mt-4 z-40">
+
+       
+      <section className="sm:flex-col lg:flex w-[70%] sm:h-auto lg:h-36 justify-center bg-white rounded-lg mt-32 z-40 absolute">
         <div className="flex max-md:flex-col max-md:items-center w-full justify-between p-8 gap-5">
           <div className="flex flex-col justify-center max-md:items-center">
             <span className="font-bold text-slate-400">IP ADDRESS</span>
@@ -76,20 +77,19 @@ function App() {
         </div>
       </section>
 
-      <div className='w-full h-full bg-green-400 -mt-[100px] z-10'>
-      {results && results.latitude !== undefined && results.longitude !== undefined && (
-        <MapContainer center={[results.latitude, results.longitude]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-            <Marker position={[results.latitude, results.longitude]}>
-            <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      )}
+    
+
+      <div className='w-full h-full z-10'>
+        {results && results.latitude !== undefined && results.longitude !== undefined && (
+          <MapContainer center={[results.latitude, results.longitude]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+              <Marker position={[results.latitude, results.longitude]}>
+            </Marker>
+          </MapContainer>
+        )}
       </div>
 
     </main>
